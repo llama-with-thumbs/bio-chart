4fbc67e7e563895bee3f2af7b51c3671fd8cdad6
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-14 16:45:07 -0400

    feat: make infinete loop of image capture

--- image_capture_scheduler.py
+++ image_capture_scheduler.py
@@ -1,5 +1,19 @@
 import schedule
 import time
 from capture_image import capture_image
+# from firebase_uploader import upload_image_to_firebase
 
 schedule.every(0.5).minutes.do(capture_image)
+
+# Keep the script running to allow scheduled tasks
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+    # Capture an image and get its path
+    image_path = capture_image()
+
+    # Check if an image has been captured
+    # if image_path is not None:
+        # Call the upload_image_to_firebase function with the captured image path
+        # upload_image_to_firebase(image_path, image_path)
