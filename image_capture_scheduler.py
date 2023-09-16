89554a16f2cc17e5e03ea9a6448314b5681df83c
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-15 21:40:44 -0400

    fix: 30 min interval

--- image_capture_scheduler.py
+++ image_capture_scheduler.py
@@ -1,21 +1,16 @@
-import schedule
 import time
 from capture_image import capture_image
 
-# Define the interval in minutes (30 minutes)
-interval_minutes = 30
+# Define the interval in seconds (30 minutes)
+interval_seconds = 30 * 60  # 30 minutes * 60 seconds/minute
 
-# Schedule the capture_image function to run at the specified interval
-schedule.every(interval_minutes).minutes.do(capture_image)
-
-# Keep the script running to allow scheduled tasks
 while True:
-    schedule.run_pending()
-    time.sleep(1)
-
     # Capture an image and get its path
     image_path = capture_image()
 
     # You can include additional logic here, such as uploading the image to Firebase
     # if image_path is not None:
     #     upload_image_to_firebase(image_path, image_path)
+
+    # Sleep for the specified interval
+    time.sleep(interval_seconds)
