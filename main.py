0079a61fec565867929eb4d1e16542888857fd2a
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-20 17:03:42 -0400

    fix: fix main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from capture_image import capture_image
+from rotate_image import rotate_image
 
 # Define the interval in seconds (10 minutes)
 interval_seconds = 10 * 60  # 10 minutes * 60 seconds/minute
@@ -7,5 +8,6 @@ interval_seconds = 10 * 60  # 10 minutes * 60 seconds/minute
 while True:
     # Capture an image and get its path
     image_path = capture_image()
+    rotate_image(image_path)
 
     time.sleep(interval_seconds)
