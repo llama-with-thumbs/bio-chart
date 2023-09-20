e5943ff333c63d65459f8dba065e5596b02974cd
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-20 15:24:00 -0400

    fix: print more camera meta data

--- camera-test.py
+++ camera-test.py
@@ -1,26 +1,43 @@
 import picamera
+import time
 
 # Initialize the camera
 camera = picamera.PiCamera()
 
-# Print Camera Settings
-print("Camera Settings:")
-print("Resolution: {}".format(camera.resolution))
-print("Exposure Mode: {}".format(camera.exposure_mode))
-print("Exposure Compensation: {}".format(camera.exposure_compensation))
-print("ISO: {}".format(camera.iso))
-print("Shutter Speed: {} microseconds".format(camera.shutter_speed))
-print("Meter Mode: {}".format(camera.meter_mode))
-print("AWB (Auto White Balance) Mode: {}".format(camera.awb_mode))
-print("Image Effect: {}".format(camera.image_effect))
-print("Contrast: {}".format(camera.contrast))
-print("Brightness: {}".format(camera.brightness))
-print("Saturation: {}".format(camera.saturation))
-print("Sharpness: {}".format(camera.sharpness))
-print("Annotate Text: {}".format(camera.annotate_text))
-print("Framerate: {}".format(camera.framerate))
-print("Rotation: {}".format(camera.rotation))
-print("Sensor Mode: {}".format(camera.sensor_mode))
+# Capture an image (this will use automatic exposure settings)
+camera.capture('auto_exposure.jpg')
+
+# Retrieve the actual exposure time in microseconds
+exposure_speed = camera.exposure_speed
+
+# Get the image metadata
+image_metadata = {
+    "Resolution": camera.resolution,
+    "Exposure Mode": camera.exposure_mode,
+    "Exposure Compensation": camera.exposure_compensation,
+    "ISO": camera.iso,
+    "Shutter Speed (microseconds)": camera.shutter_speed,
+    "Meter Mode": camera.meter_mode,
+    "AWB (Auto White Balance) Mode": camera.awb_mode,
+    "Image Effect": camera.image_effect,
+    "Contrast": camera.contrast,
+    "Brightness": camera.brightness,
+    "Saturation": camera.saturation,
+    "Sharpness": camera.sharpness,
+    "Annotate Text": camera.annotate_text,
+    "Framerate": camera.framerate,
+    "Rotation": camera.rotation,
+    "Sensor Mode": camera.sensor_mode,
+}
 
 # Close the camera
 camera.close()
+
+# Save the image metadata to a text file
+with open('image_metadata.txt', 'w') as metadata_file:
+    for key, value in image_metadata.iteritems():
+        metadata_file.write("{}: {}\n".format(key, value))
+
+# Print the exposure settings for the captured image
+print("Exposure Settings for Captured Image:")
+print("Exposure Speed for Captured Image: {} microseconds".format(exposure_speed))
