54aebf4704a6262e67ba367168b76953a02faaf1
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-20 15:28:12 -0400

    feat: pritn vharacteristics and values

--- camera-test.py
+++ camera-test.py
@@ -6,10 +6,12 @@ camera = picamera.PiCamera()
 # Get a list of all camera characteristics
 characteristics = dir(camera)
 
-# Print the list of characteristics
-print("List of Camera Characteristics:")
+# Print the list of characteristics and their values
+print("List of Camera Characteristics and Their Values:")
 for char in characteristics:
-    print(char)
+    if not callable(getattr(camera, char)):  # Check if it's not a method
+        value = getattr(camera, char)
+        print("{}: {}".format(char, value))
 
 # Close the camera
 camera.close()
