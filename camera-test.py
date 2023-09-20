8c6b891993ea44d3f89c5dac1dd721889af733bc
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-20 15:26:36 -0400

    feat: list characteristics

--- camera-test.py
+++ camera-test.py
@@ -1,43 +1,15 @@
 import picamera
-import time
 
 # Initialize the camera
 camera = picamera.PiCamera()
 
-# Capture an image (this will use automatic exposure settings)
-camera.capture('auto_exposure.jpg')
+# Get a list of all camera characteristics
+characteristics = dir(camera)
 
-# Retrieve the actual exposure time in microseconds
-exposure_speed = camera.exposure_speed
-
-# Get the image metadata
-image_metadata = {
-    "Resolution": camera.resolution,
-    "Exposure Mode": camera.exposure_mode,
-    "Exposure Compensation": camera.exposure_compensation,
-    "ISO": camera.iso,
-    "Shutter Speed (microseconds)": camera.shutter_speed,
-    "Meter Mode": camera.meter_mode,
-    "AWB (Auto White Balance) Mode": camera.awb_mode,
-    "Image Effect": camera.image_effect,
-    "Contrast": camera.contrast,
-    "Brightness": camera.brightness,
-    "Saturation": camera.saturation,
-    "Sharpness": camera.sharpness,
-    "Annotate Text": camera.annotate_text,
-    "Framerate": camera.framerate,
-    "Rotation": camera.rotation,
-    "Sensor Mode": camera.sensor_mode,
-}
+# Print the list of characteristics
+print("List of Camera Characteristics:")
+for char in characteristics:
+    print(char)
 
 # Close the camera
 camera.close()
-
-# Save the image metadata to a text file
-with open('image_metadata.txt', 'w') as metadata_file:
-    for key, value in image_metadata.iteritems():
-        metadata_file.write("{}: {}\n".format(key, value))
-
-# Print the exposure settings for the captured image
-print("Exposure Settings for Captured Image:")
-print("Exposure Speed for Captured Image: {} microseconds".format(exposure_speed))
