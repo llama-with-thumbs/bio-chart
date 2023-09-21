28802511e6c69afe21e5be9fe5e5ee2444472100
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-21 13:41:35 -0400

    fix: coamer-test

--- camera-test.py
+++ camera-test.py
@@ -1,17 +1,22 @@
 import picamera
 
-# Initialize the camera
-camera = picamera.PiCamera()
+def capture_image(image_filename):
+    try:
+        # Initialize the camera
+        with picamera.PiCamera() as camera:
+            # Set the resolution (adjust as needed)
+            camera.resolution = (1920, 1080)  # Example resolution
 
-# Get a list of all camera characteristics
-characteristics = dir(camera)
+            # Capture the image and save it
+            camera.capture(image_filename)
+            print(f"Image saved as {image_filename}")
 
-# Print the list of characteristics and their values
-print("List of Camera Characteristics and Their Values:")
-for char in characteristics:
-    if not callable(getattr(camera, char)):  # Check if it's not a method
-        value = getattr(camera, char)
-        print("{}: {}".format(char, value))
+    except Exception as e:
+        print(f"Error: {e}")
 
-# Close the camera
-camera.close()
+if __name__ == "__main__":
+    # Specify the image filename
+    image_filename = "my_image.jpg"
+
+    # Call the capture_image function with the desired filename
+    capture_image(image_filename)
