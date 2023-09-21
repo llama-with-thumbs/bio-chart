1e08d20502785467ab6ef4a7b18717e0b510a31a
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-21 11:16:41 -0400

    fix: fix app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import time
 from capture_image import capture_image
-# from firebase_uploader import upload_image_to_firebase
 
 # Define the interval in seconds (10 minutes)
 interval_seconds = 10 * 60  # 10 minutes * 60 seconds/minute
@@ -8,6 +7,5 @@ interval_seconds = 10 * 60  # 10 minutes * 60 seconds/minute
 while True:
     # Capture an image and get its path
     image_path = capture_image()
-    # upload_image_to_firebase(image_path, image_path)
-
+    
     time.sleep(interval_seconds)
