6baa75588988ed7a51e1c8b5cab9f45f14bda33a
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-09-21 13:50:35 -0400

    fix: opencv

--- camera-test.py
+++ camera-test.py
@@ -3,14 +3,21 @@ import cv2
 # Initialize the camera
 camera = cv2.VideoCapture(0)
 
-# Capture a single frame
-ret, frame = camera.read()
-
-# Save the captured frame as an image
-if ret:
-    cv2.imwrite('my_image.jpg', frame)
+# Check if the camera opened successfully
+if not camera.isOpened():
+    print("Error: Could not open camera.")
 else:
-    print('Error capturing image')
+    # Capture a single frame from the camera
+    ret, frame = camera.read()
+
+    # Check if the frame was captured successfully
+    if ret:
+        # Save the captured frame as an image file
+        image_filename = "my_image.jpg"
+        cv2.imwrite(image_filename, frame)
+        print(f"Image saved as {image_filename}")
+    else:
+        print("Error: Could not capture image.")
 
 # Release the camera
 camera.release()
