094869b0e2d62baf4250478f3ed63749708e1229
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-11-07 14:04:31 -0500

    feat: upload raw image into storage and update firestore

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from firebase_uploader import upload_image_to_firebase
 from rotate_image import rotate_image
 from cut_and_save_rectangle import cut_and_save_rectangle
 from update_latest_image import update_latest_image
+from upload_raw_image import upload_raw_image
 
 # Define the interval in seconds (10 minutes)
 interval_seconds = 10 * 60  # 10 minutes * 60 seconds/minute
@@ -16,22 +17,29 @@ y=890
 # Define the rotation angle
 rotation_angle = -2.5  # Rotation angle in degrees
 
+# Define chamber name
+chamber = "CHA-AFBEFC"
+
 while True:
     # Capture an image and get its path
-    image_path = capture_image()
-    rotate_image(image_path, rotation_angle)
 
-    # Call the cut_and_save_rectangle function for each image
-    image_path_a = cut_and_save_rectangle(image_path, 1010, y , width, height, "A")
-    image_path_b = cut_and_save_rectangle(image_path, 1660, y , width, height, "B")
-    image_path_c = cut_and_save_rectangle(image_path, 300, y , width, height, "C")
+    upload_raw_image("captured_images/CHA-AFBEFC/Raw images/image_captured_image_2023-09-21_16-06-07.jpg", chamber)
 
-    update_latest_image(image_path_a)
-    update_latest_image(image_path_b)
-    update_latest_image(image_path_c)
+    # image_path = capture_image()
 
-    upload_image_to_firebase(image_path_a, image_path_a)
-    upload_image_to_firebase(image_path_b, image_path_b)
-    upload_image_to_firebase(image_path_c, image_path_c)
+    # rotate_image(image_path, rotation_angle)
+
+    # # Call the cut_and_save_rectangle function for each image
+    # image_path_a = cut_and_save_rectangle(image_path, 1010, y , width, height, "A")
+    # image_path_b = cut_and_save_rectangle(image_path, 1660, y , width, height, "B")
+    # image_path_c = cut_and_save_rectangle(image_path, 300, y , width, height, "C")
+
+    # update_latest_image(image_path_a)
+    # update_latest_image(image_path_b)
+    # update_latest_image(image_path_c)
+
+    # upload_image_to_firebase(image_path_a, image_path_a)
+    # upload_image_to_firebase(image_path_b, image_path_b)
+    # upload_image_to_firebase(image_path_c, image_path_c)
 
     time.sleep(interval_seconds)
---
+++ upload_raw_image.py
@@ -0,0 +1,46 @@
+import os
+import firebase_admin
+import datetime
+from firebase_admin import credentials, storage, firestore
+from datetime import datetime
+
+def upload_raw_image(image_path, chamber):
+    # Initialize Firebase Admin SDK with credentials
+    cred = credentials.Certificate("bio-chart-firebase.json")
+    firebase_admin.initialize_app(cred, {"storageBucket": "bio-chart.appspot.com"})
+
+    # Reference to the Firebase Storage bucket
+    bucket = storage.bucket()
+
+    # Extract the file name from the firebase_image_path
+    file_name = os.path.basename(image_path)
+
+    # Define the category and updated firebase_image_path
+    category = f"{chamber}/Raw images"
+    firebase_image_path = f"{category}/{file_name}"
+
+    # Upload the image to Firebase Storage
+    blob = bucket.blob(firebase_image_path.replace("\\", "/"))
+    blob.upload_from_filename(image_path.replace("\\", "/"), content_type="image/jpeg")
+
+    print(f"Image uploaded to Firebase Storage at '{firebase_image_path}'")
+
+    # Create a Firestore client
+    db = firestore.client()
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    new_document = {
+        "creation date": timestamp,
+        "path": firebase_image_path
+    }
+
+    # Add the new document to the specified collection
+    db.collection('bio-chart').document(chamber).collection('Raw images').add(new_document)
+
+    print("Document added successfully.")
+
+    # End the Firebase session
+    firebase_admin.delete_app(firebase_admin.get_app())
+# Example usage:
+# save_raw_image("local_image.jpg", "remote_image.jpg")
