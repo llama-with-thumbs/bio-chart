f27ea1b91e7668bdf9698b25d752cdb56f5ecd28
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-11-08 10:04:37 -0500

    fix: delete datetime duble

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from cut_and_save_snippet import cut_and_save_snippet
 # from update_latest_image import update_latest_image
 from upload_raw_image import upload_raw_image
 from datetime import datetime
+from calculate_intensity import calculate_intensity
 
 # Define the interval in seconds (30 minutes)
 interval_seconds = 30 * 60  # 30 minutes * 60 seconds/minute
@@ -29,7 +30,7 @@ flask_c = "FLA-D3610A"
 
 while True:
     # Capture an image and get its path
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     image_path = capture_image(timestamp)
 
@@ -46,7 +47,7 @@ while True:
     # update_latest_image(image_path_b)
     # update_latest_image(image_path_c)
 
-    upload_snippet_to_firebase(snippet_path_a, flask_a, chamber, timestamp)
+    upload_snippet_to_firebase(snippet_path_a, flask_a, chamber, timestamp, calculate_intensity(snippet_path_a))
     upload_snippet_to_firebase(snippet_path_b, flask_b, chamber, timestamp)
     upload_snippet_to_firebase(snippet_path_c, flask_c, chamber, timestamp)
 
