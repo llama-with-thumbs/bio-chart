c2b642dffb71711d1fb5886478208d44be243fa9
Vivi Filitovich <vlad.filitovich@gmail.com>
2024-08-22 20:57:16 -0400

    feat: change position of the cut image

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ interval_seconds = 30 * 60  # 30 minutes * 60 seconds/minute
 
 # Define the coordinates for cropping
 # x, y, width, height
-coordinates_b = [1558 ,918 ,386 ,489]
+coordinates_b = [1658 ,998 ,386 ,489]
 
 # Define the rotation angle
 rotation_angle = 180  # Rotation angle in degrees
