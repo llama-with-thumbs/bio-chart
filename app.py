93c9aebbc1119f626b9c6293b3d5e73db3666a9b
Vivi Filitovich <vlad.filitovich@gmail.com>
2024-08-29 11:47:11 -0400

    feat: add secound coorditates

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ interval_seconds = 30 * 60  # 30 minutes * 60 seconds/minute
 # Define the coordinates for cropping
 # x, y, width, height
 coordinates_b = [1200, 798, 425, 530]
+coordinates_c = [1700, 868 ,425 ,530]
 
 # Define the rotation angle
 rotation_angle = 180  # Rotation angle in degrees
@@ -23,6 +24,7 @@ chamber = "CHA-8BEA5D1"
 
 # Define flasks names
 flask_b = "SMP-A0018B"
+flask_c = "FLA-5B4CD"
 
 while True:
     # Capture an image and get its path
@@ -36,7 +38,10 @@ while True:
 
     # # Call the cut_and_save_rectangle function for each image
     snippet_path_b = cut_and_save_snippet(image_path, coordinates_b, flask_b, chamber)
+    snippet_path_c = cut_and_save_snippet(image_path, coordinates_c, flask_c, chamber)
 
     upload_snippet_to_firebase(snippet_path_b, flask_b, chamber, timestamp, calculate_mean_intensities(snippet_path_b))
+    upload_snippet_to_firebase(snippet_path_c, flask_c, chamber, timestamp, calculate_mean_intensities(snippet_path_c))
+
 
     time.sleep(interval_seconds)
