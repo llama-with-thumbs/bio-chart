cb07f63d66fb1865346fcf52dc8d50714a9bbfa9
Vivi Filitovich <vlad.filitovich@gmail.com>
2024-09-02 17:38:18 -0400

    fix: gif

--- app_test.py
+++ app_test.py
@@ -1,8 +1,8 @@
 from compile_images_to_video import compile_images_into_video
 from firebase_storage_image_downloader import download_images_from_firebase_folder
 from create_gif_from_images import create_gif_from_images
-sample = "FLA-5B4CD"
+sample = "SMP-A0018B"
 
 # download_images_from_firebase_folder(f"captured_images/{sample}", f"captured_images/{sample}")
 # compile_images_into_video(f"captured_images/{sample}", f"captured_images/{sample}.avi", 20)
-create_gif_from_images(f"SMP-A0018B/{sample}", f"{sample}.gif", 200, 1, 1)
+create_gif_from_images(f"CHA-8BEA5D1/{sample}", f"{sample}.gif", 200, 1, 1)
