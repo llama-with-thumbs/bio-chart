9c4853fa2b9ec86c5004217b726344f30b6802b4
Vivi Filitovich <vlad.filitovich@gmail.com>
2024-09-21 15:00:23 -0400

    feat: start new sample

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ rotation_angle = 1.2  # Rotation angle in degrees
 chamber = "CHA-AFBEFC"
 
 # Define flasks names
-flask_a = "FLA-2783C2"
-flask_b = "FLA-74F078"
-flask_c = "FLA-D3610A"
+flask_a = "SMP-8D8FDF"
 
 while True:
     # Capture an image and get its path
@@ -40,15 +38,11 @@ while True:
 
     # # Call the cut_and_save_rectangle function for each image
     snippet_path_a = cut_and_save_snippet(image_path, coordinates_a, flask_a, chamber)
-    snippet_path_b = cut_and_save_snippet(image_path, coordinates_b, flask_b, chamber)
-    snippet_path_c = cut_and_save_snippet(image_path, coordinates_c, flask_c, chamber)
 
     # update_latest_image(image_path_a)
     # update_latest_image(image_path_b)
     # update_latest_image(image_path_c)
 
     upload_snippet_to_firebase(snippet_path_a, flask_a, chamber, timestamp, calculate_mean_intensities(snippet_path_a))
-    upload_snippet_to_firebase(snippet_path_b, flask_b, chamber, timestamp, calculate_mean_intensities(snippet_path_b))
-    upload_snippet_to_firebase(snippet_path_c, flask_c, chamber, timestamp, calculate_mean_intensities(snippet_path_c))
 
     time.sleep(interval_seconds)
