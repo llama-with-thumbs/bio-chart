0f20fd7197c802e28bab4de81dbb4584b1400b31
Vivi Filitovich <vlad.filitovich@gmail.com>
2024-12-14 10:18:37 -0500

    init: start new chamber

--- app.py
+++ app.py
@@ -11,24 +11,20 @@ from upload_gif_file import upload_gif_file
 from calculate_green_object_area import calculate_green_object_area
 
 # Define the interval in seconds (30 minutes)
-interval_seconds = 30 * 60  # 30 minutes * 60 seconds/minute
+interval_seconds = 10 * 60  # 30 minutes * 60 seconds/minute
 
 # Define the coordinates for cropping
 # x, y, width, height
 coordinates_a = [530, 568, 425, 480]
-coordinates_b = [1060, 578, 425, 480]
-coordinates_c = [1570, 578, 425, 480]
 
 # Define the rotation angle
 rotation_angle = 1.5  # Rotation angle in degrees
 
 # Define chamber name
-chamber = "CHA-AFBEFC"
+chamber = "CHA-DDFBE6"
 
 # Define flasks names
-flask_a = "FLA-7E6827"
-flask_b = "FLA-29C14C"
-flask_c = "FLA-8CDE9E"
+flask_a = "SMP-4FF31C"
 
 while True:
     timestamp = datetime.now().isoformat()
@@ -41,20 +37,11 @@ while True:
 
     # # Call the cut_and_save_rectangle function for each image
     snippet_path_a = cut_and_save_snippet(image_path, coordinates_a, flask_a, chamber)
-    snippet_path_b = cut_and_save_snippet(image_path, coordinates_b, flask_b, chamber)
-    snippet_path_c = cut_and_save_snippet(image_path, coordinates_c, flask_c, chamber)
 
     upload_snippet_to_firebase(snippet_path_a, flask_a, chamber, timestamp, calculate_mean_intensities(snippet_path_a), calculate_green_object_area(snippet_path_a))
-    upload_snippet_to_firebase(snippet_path_b, flask_b, chamber, timestamp, calculate_mean_intensities(snippet_path_b), calculate_green_object_area(snippet_path_b))
-    upload_snippet_to_firebase(snippet_path_c, flask_c, chamber, timestamp, calculate_mean_intensities(snippet_path_c), calculate_green_object_area(snippet_path_c))
-
+  
     create_gif_from_images(f"{chamber}/{flask_a}", f"{flask_a}.gif", 200, 0.1, 1)
-    create_gif_from_images(f"{chamber}/{flask_b}", f"{flask_b}.gif", 200, 0.1, 1)
-    create_gif_from_images(f"{chamber}/{flask_c}", f"{flask_c}.gif", 200, 0.1, 1)
-
-
+    
     upload_gif_file(f"output_gif_folder/{flask_a}.gif", chamber, flask_a)
-    upload_gif_file(f"output_gif_folder/{flask_b}.gif", chamber, flask_b)
-    upload_gif_file(f"output_gif_folder/{flask_c}.gif", chamber, flask_c)
-
+   
     time.sleep(interval_seconds)
