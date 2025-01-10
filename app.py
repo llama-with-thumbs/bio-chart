858069ca6d7ad205d172fd3c7dec034eb332705c
Vivi Filitovich <vlad.filitovich@gmail.com>
2025-01-10 13:17:16 -0500

    feat: skip every second image. not print gif files list.

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from create_gif_from_images import create_gif_from_images
 from upload_gif_file import upload_gif_file
 from calculate_green_object_area import calculate_green_object_area
 
-# Define the interval in seconds (10 minutes)
-interval_seconds = 30 * 60  # 10 minutes * 60 seconds/minute
+# Define the interval in seconds (30 minutes)
+interval_seconds = 30 * 60  # 30 minutes * 60 seconds/minute
 
 # Define the coordinates for cropping
 # x, y, width, height
@@ -28,24 +28,24 @@ chamber = "CHA-146658"
 flask_b = "SMP-96A579"
 
 while True:
-    # timestamp = datetime.now().isoformat()
+    timestamp = datetime.now().isoformat()
 
-    # image_path = capture_image(timestamp)
+    image_path = capture_image(timestamp)
 
-    # rotate_image(image_path, rotation_angle)
+    rotate_image(image_path, rotation_angle)
 
-    # upload_raw_image(image_path, chamber, timestamp)
+    upload_raw_image(image_path, chamber, timestamp)
 
-    # # Call the cut_and_save_rectangle function for each image
+    # Call the cut_and_save_rectangle function for each image
     # snippet_path_a = cut_and_save_snippet(image_path, coordinates_a, flask_a, chamber)
-    # snippet_path_b = cut_and_save_snippet(image_path, coordinates_b, flask_b, chamber)
+    snippet_path_b = cut_and_save_snippet(image_path, coordinates_b, flask_b, chamber)
 
 
     # upload_snippet_to_firebase(snippet_path_a, flask_a, chamber, timestamp, calculate_mean_intensities(snippet_path_a), calculate_green_object_area(snippet_path_a))
-    # upload_snippet_to_firebase(snippet_path_b, flask_b, chamber, timestamp, calculate_mean_intensities(snippet_path_b), calculate_green_object_area(snippet_path_b))
+    upload_snippet_to_firebase(snippet_path_b, flask_b, chamber, timestamp, calculate_mean_intensities(snippet_path_b), calculate_green_object_area(snippet_path_b))
 
     # create_gif_from_images(f"{chamber}/{flask_a}", f"{flask_a}.gif", 200, 0.1, 1)
-    create_gif_from_images(f"{chamber}/{flask_b}", f"{flask_b}.gif", 200, 0.1, 3)
+    create_gif_from_images(f"{chamber}/{flask_b}", f"{flask_b}.gif", 200, 0.1, 2)
 
     
     # upload_gif_file(f"output_gif_folder/{flask_a}.gif", chamber, flask_a)
